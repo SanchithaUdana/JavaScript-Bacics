d55effe6f2eb7a3060922e26475a5f500fff393d
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-06-11 19:53:51 +0530

    js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // print console output
-console.log('welcome to javascript');
+//console.log('welcome to javascript');
 
 // pop up a alert box
-alert('welcome');
+//alert('welcome');
 
 // get user input
 //const name = prompt('Enter your name');
@@ -12,79 +12,76 @@ alert('welcome');
 //console.log('welcome ' + name + ' you are ' + age + ' years old');
 //console.log(`welcome ${name} your age is ${age}`);
 
-let a,b,c;
-a=b=c = 20;
+let a, b, c;
+a = b = c = 20;
 
-const myname = 'sanchitha udana wijesundara';
+const myname = "sanchitha udana wijesundara";
 console.log(myname + a + b + c);
 
 console.log(myname[0]); // print string elements
-console.log(myname.substring(0,4));
-console.log(myname.split(' '));
-console.log(myname.lastIndexOf('a'));
+console.log(myname.substring(0, 4));
+console.log(myname.split(" "));
+console.log(myname.lastIndexOf("a"));
 console.log(myname.charAt(3));
-console.log(myname.endsWith('dara'));
-console.log(myname.replace('sanchitha' , 'Sanchitha'));
-
+console.log(myname.endsWith("dara"));
+console.log(myname.replace("sanchitha", "Sanchitha"));
 
 let details = {
-    name: 'sanchitha',
-    age : 20,
-    languages : ['java' , 'javascript', 'python', 'c', 'html'],
+  name: "sanchitha",
+  age: 20,
+  languages: ["java", "javascript", "python", "c", "html"],
 
-    seField : {
-        ios: 'swift',
-        web : 'react',
-    }
-
+  seField: {
+    ios: "swift",
+    web: "react",
+  },
 };
 
 console.log(details.seField.ios);
 
-details.school = 'vns';
+details.school = "vns";
 details.department = "ict";
 console.log(details);
 
 // functions creating
 
-function displayName(){
-    const name = prompt('Enter your Name ');
-    console.log(name);
+function displayName() {
+  const name = prompt("Enter your Name ");
+  console.log(name);
 }
 
 // function calling
-displayName();
-
+//displayName();
 
 // arrorw functions
 
-let arrfunc = (name , age) => {
-    console.log('this is arrow function');
-    console.log(`hi ${name} you are ${age} years old`);
-}
+let arrfunc = (name, age) => {
+  console.log("this is arrow function");
+  console.log(`hi ${name} you are ${age} years old`);
+};
 
-arrfunc('udana', 20);
+arrfunc("udana", 20);
 
-// if arrow function has only one parameter, 
-    // we can use this method to write arrow function
+// if arrow function has only one parameter,
+// we can use this method to write arrow function
 
-let newFun = name => {
-    console.log(`how are you ${name}`);
-}
+let newFun = (name) => {
+  console.log(`how are you ${name}`);
+};
 
-newFun('sanchitha');
+newFun("sanchitha");
 
 // if arrow function have only one parameter and one statement
-    // we can write
+// we can write
 
-let newFun2 = name => console.log(name);
+let newFun2 = (name) => console.log(name);
 
-// with only return 
-let newFun3 = name => `your name is ${name}`; 
+// with only return
+let newFun3 = (name) => `your name is ${name}`;
 
-newFun2('amal');
+newFun2("amal");
 
-console.log(newFun3('kamal'));
+console.log(newFun3("kamal"));
 
 console.error("js error");
 console.warn("js warning");
@@ -94,12 +91,13 @@ console.log(details);
 var student1 = new Object();
 var student2 = {};
 var student3 = {
-    "name":"sanchitha",
-    "age":21,
-    "dep":"ict"
+  name: "sanchitha",
+  age: 21,
+  dep: "ict",
 };
 
-console.log(student3.name);
+student2.addingNum = function () {
+  console.log("hello this is function in an object");
+};
 
-
-
+student2.addingNum();
